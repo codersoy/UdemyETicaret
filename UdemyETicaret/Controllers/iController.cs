ffc499dde39729d24a19c712df5c638e4bd75e61
codersoy <49654518+codersoy@users.noreply.github.com>
2019-06-15 12:24:46 +0300

    Importing project

---
+++ UdemyETicaret/Controllers/iController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UdemyETicaret.DB;
+
+namespace UdemyETicaret.Controllers
+{
+    public class iController : Controller
+    {
+        ETicaretEntities context;
+        public iController()
+        {
+            context = new ETicaretEntities();
+        }
+        // GET: i
+        public ActionResult Index(int id)
+        {
+
+            IQueryable<DB.Products> products = context.Products;
+            DB.Categories category = null;
+
+            if (id == 0)
+            {
+                products = products;
+                category = category;
+            }
+           
+            var viewModel = new Models.i.IndexModel()
+            {
+                Products = products.ToList(),
+                Category = category
+            };
+
+
+            return View(viewModel);
+        }
+    }
+}
